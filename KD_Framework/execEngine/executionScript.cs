d337d2570295b2b07167334fa6b36727c45816dc
Tamás Rácz <tamas.racz@capgemini.com>
2019-05-06 12:07:06 +0200

    execScript

--- KD_Framework/execEngine/executionScript.cs
+++ KD_Framework/execEngine/executionScript.cs
@@ -13,8 +13,123 @@ using OpenQA.Selenium.Chrome;
 namespace KD_Framework.execEngine
 {
     [TestClass]
-    class ExecutionScript
+    public class ExecutionScript
     {
+        public static ActionKeywords actionKeywords;
+        public static string sActionKeyword;
+        public static string sPageObject;
+        //reflection class object
+        public static MethodInfo methodInfo;
 
+        public static int iTestStep;
+        public static int iTestLastStep;
+        public static string sTestCaseID;
+        public static string sRunMode;
+        public static string sData;
+        public static bool bResult;
+
+        [TestMethod]
+        public void TestMain()
+        {
+            actionKeywords = new ActionKeywords();
+
+            //path of the xls
+            string sPath = Constants.testDataPath;
+
+            XLUtils.setXlFile(sPath);
+
+            execute_TestCase();
+        }
+
+        private void execute_TestCase()
+        {
+            //get total testcase num
+            int iTotalTestCases = XLUtils.getRowCount(Constants.Sheet_TestCases);
+            //the loop will execute the number of times equal to total number of test cases
+            for (int iTestCase = 1; iTestCase <= iTotalTestCases; iTestCase++)
+            {
+                bResult = true;
+                sTestCaseID = XLUtils.getCellData(iTestCase, Constants.Col_TestCaseID, Constants.Sheet_TestCases);
+
+                sRunMode = XLUtils.getCellData(iTestCase, Constants.Col_RunMode, Constants.Sheet_TestCases);
+
+                if (sRunMode != null && sRunMode.Equals("Yes"))
+                {
+                    iTestStep = XLUtils.getRowContains(sTestCaseID, Constants.Col_TestCaseID, Constants.Sheet_TestSteps);
+
+                    iTestLastStep = XLUtils.getTestStepsCount(Constants.Sheet_TestSteps, sTestCaseID, iTestStep);
+
+                    bResult = true;
+
+                    for (; iTestStep <= iTestLastStep; iTestStep++)
+                    {
+                        sActionKeyword = XLUtils.getCellData(iTestStep, Constants.Col_ActionKeyword, Constants.Sheet_TestSteps);
+
+                        sPageObject = XLUtils.getCellData(iTestStep, Constants.Col_PageObject, Constants.Sheet_TestSteps);
+
+                        sData = XLUtils.getCellData(iTestStep, Constants.Col_DataSet, Constants.Sheet_TestSteps);
+                        execute_Actions();
+
+                        if (!bResult)
+                        {
+                            XLUtils.setCellData(Constants.KEYWORD_FAIL, iTestCase, Constants.Col_CaseResult, Constants.Sheet_TestCases);
+                            break;
+                        }
+                    }
+
+                    if (bResult)
+                    {
+                        XLUtils.setCellData(Constants.KEYWORD_PASS, iTestCase, Constants.Col_CaseResult, Constants.Sheet_TestCases);
+                    }
+                }
+            }            
+        }
+
+        private static void execute_Actions()
+        {
+            string sResult;
+
+            if (sActionKeyword != null)
+            {
+                MethodInfo mi = actionKeywords.GetType().GetMethod(sActionKeyword);
+
+                string[] args = { sPageObject, sData };
+                mi.Invoke(sActionKeyword, args);
+
+                sResult = bResult ? Constants.KEYWORD_PASS : Constants.KEYWORD_FAIL;
+
+                XLUtils.setCellData(sResult, iTestStep, Constants.Col_TestStepResult, Constants.Sheet_TestSteps);
+                if (!bResult)
+                {
+                    ActionKeywords.closeBrowser("", "");
+                }
+            }
+        }
+
+        [ClassCleanup]
+        public void TestCloseApp()
+        {
+            XLUtils.xlWB.Save();
+            XLUtils.xlWB.Close(0);
+            XLUtils.xlApp.Quit();
+        }
+
+        [ClassInitialize]
+        public void TestSetup()
+        {
+
+        }
+
+        [TestInitialize]
+        public void InitTest()
+        {
+
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
+
+        }
     }
 }
